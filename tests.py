5878a357aa67b6fe6776e5671a57fc553e6fc125
Nuikin Denis <dnsurgai@yandex.ru>
2024-05-10 18:44:10 +0400

    add test.py and readme

--- tests.py
+++ tests.py
@@ -1,24 +1,73 @@
 from main import BooksCollector
+import pytest
+class Test1BooksCollector:
+    @pytest.mark.parametrize("name", ['Несуществующая1', 'Несуществующая2'])
+    def test_add_new_book_lenlimit40(self, name):
+        self.books_genre = {'Мимино': 'Комедии', 'В пасти безумия': 'Ужасы'}
+        collector_ut1 = BooksCollector()
+        collector_ut1.add_new_book(name)
+        assert name not in self.books_genre.keys() and len(name) < 41
+class Test2BooksCollector:
+    @pytest.mark.parametrize("name", ['В пасти безумия', 'Мимино'])
+    @pytest.mark.parametrize("genre", ['Ужасы', 'Комедии'])
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-class TestBooksCollector:
+    def test_set_book_genre_exist_in_books_genre(self, name, genre):
+        collector_ut2 = BooksCollector()
+        self.books_genre = {'Мимино': 'Комедии', 'В пасти безумия': 'Ужасы' }
+        self.genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        collector_ut2.set_book_genre(name, genre)
+        assert name in self.books_genre.keys() and genre in self.genre
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+class Test3BookCollector:
+    @pytest.mark.parametrize("name", ['бук'])
+    def test_get_books_genre_true_genre(self, name):
+        self.books_genre = {'бук': 'Жанр'}
+        collector_ut3 = BooksCollector()
+        assert collector_ut3.get_books_genre() != 'Жанр'
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+class Test4BookCollector:
+    @pytest.mark.parametrize("genre", ['Ужасы'])
+    def test_get_books_with_specific_genre_true_specific (self, genre):
+        collector_ut4 = BooksCollector()
+        assert collector_ut4.get_books_with_specific_genre(genre) != 'Ужасы'
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+class Test5BookCollector:
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_books_genre_true_dict (self):
+        self.books_genre = {'В пасти безумия': 'Ужасы'}
+        collector_ut5 = BooksCollector()
+        assert collector_ut5.get_books_genre() != {'В пасти безумия': 'Ужасы'}
+
+class Test6BooksCollector:
+    def test_get_books_for_children_notadult(self):
+        collector_ut6 = BooksCollector()
+        self.books_genre = {'В пасти безумия':'Ужасы', 'Мимино':'Комедии'}
+        self.genre_age_rating = ['Ужасы', 'Детективы']
+        books_for_children = ['Мимино']
+        collector_ut6.get_books_for_children()
+        for i in range(0, len(books_for_children)):
+            book = books_for_children[i]
+            assert self.books_genre.get(book) != 'Ужасы' and self.books_genre.get(book) != 'Детективы'
+class Test7BooksCollector:
+    @pytest.mark.parametrize("name", ['Несуществующая1', 'Несуществующая2'])
+    def test_add_book_in_favorites_not_exist_books_genre(self, name):
+        self.books_genre = {'В пасти безумия': 'Ужасы', 'Мимино': 'Комедии'}
+        collector_ut7 = BooksCollector()
+        collector_ut7.add_book_in_favorites(name)
+        assert name not in self.books_genre.keys()
+
+class Test8BooksCollector:
+    @pytest.mark.parametrize("name", ['Мимино', 'В пасти безумия'])
+    def test_delete_book_from_favorites_exist_in_genre(self, name):
+        self.books_genre = {'В пасти безумия': 'Ужасы', 'Мимино': 'Комедии'}
+        self.favorites = ['В пасти безумия', 'Мимино']
+        collector_ut8 = BooksCollector()
+        collector_ut8.delete_book_from_favorites(name)
+        assert name in self.favorites
+
+
+class Test9BookCollector:
+    def test_get_list_of_favorites_books_true_dict (self):
+        self.favorites = ['Мимино']
+        collector_ut9 = BooksCollector()
+        assert collector_ut9.get_list_of_favorites_books() != ['Мимино']
